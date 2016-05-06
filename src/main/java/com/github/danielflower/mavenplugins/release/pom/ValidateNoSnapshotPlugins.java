8bd2692a23f03c59615290a1119e7ae849f182bf
Roland Hauser <sourcepond@gmail.com>
2016-05-06 10:07:28 +0200

    Renamed class, added unit tests

---
+++ src/main/java/com/github/danielflower/mavenplugins/release/pom/ValidateNoSnapshotPlugins.java
@@ -0,0 +1,44 @@
+package com.github.danielflower.mavenplugins.release.pom;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.apache.maven.model.Plugin;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * Validates that POM to be release does not refer a plugin with a snapshot
+ * version. The only exception is the multi-module-maven-release-plugin itself
+ * (necessary for testing).
+ *
+ */
+@Named
+@Singleton
+public class ValidateNoSnapshotPlugins extends Command {
+	static final String ERROR_FORMAT = "%s references plugin %s %s";
+	static final String MULTI_MODULE_MAVEN_PLUGIN_GROUP_ID = "com.github.danielflower.mavenplugins";
+	static final String MULTI_MODULE_MAVEN_PLUGIN_ARTIFACT_ID = "multi-module-maven-release-plugin";
+
+	@Inject
+	ValidateNoSnapshotPlugins(final Log log) {
+		super(log);
+	}
+
+	@Override
+	public void alterModel(final Context updateContext) {
+		final MavenProject project = updateContext.getProject();
+		for (final Plugin plugin : project.getModel().getBuild().getPlugins()) {
+			final String version = plugin.getVersion();
+			if (isSnapshot(version) && !isMultiModuleReleasePlugin(plugin)) {
+				updateContext.addError(ERROR_FORMAT, project.getArtifactId(), plugin.getArtifactId(), version);
+			}
+		}
+	}
+
+	private static boolean isMultiModuleReleasePlugin(final Plugin plugin) {
+		return MULTI_MODULE_MAVEN_PLUGIN_GROUP_ID.equals(plugin.getGroupId())
+				&& MULTI_MODULE_MAVEN_PLUGIN_ARTIFACT_ID.equals(plugin.getArtifactId());
+	}
+}
