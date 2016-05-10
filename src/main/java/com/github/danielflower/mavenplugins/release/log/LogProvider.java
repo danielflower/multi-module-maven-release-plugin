b693b3620baa02e1f78022ac1047ed51c9f75a55
Roland Hauser <sourcepond@gmail.com>
2016-05-10 16:05:27 +0200

    Removed Guice dependency

--- src/main/java/com/github/danielflower/mavenplugins/release/log/LogProvider.java
+++ src/main/java/com/github/danielflower/mavenplugins/release/log/LogProvider.java
@@ -1,29 +1,104 @@
 package com.github.danielflower.mavenplugins.release.log;
 
-import static java.lang.reflect.Proxy.newProxyInstance;
-
-import java.lang.reflect.InvocationHandler;
-
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.apache.maven.plugin.logging.Log;
 
-import com.google.inject.Provider;
-
 @Named
 @Singleton
-final class LogProvider implements Provider<Log> {
-	private final Log log;
+final class LogProvider implements Log, LogHolder {
+	private Log delegate;
 
-	@Inject
-	LogProvider(final InvocationHandler handler) {
-		log = (Log) newProxyInstance(getClass().getClassLoader(), new Class<?>[] { Log.class }, handler);
+	@Override
+	public void setLog(final Log log) {
+		delegate = log;
+	}
+
+	private Log getLog() {
+		if (delegate == null) {
+			throw new IllegalStateException("Delegate logger has not been set!");
+		}
+		return delegate;
 	}
 
 	@Override
-	public Log get() {
-		return log;
+	public boolean isDebugEnabled() {
+		return getLog().isDebugEnabled();
+	}
+
+	@Override
+	public void debug(final CharSequence content) {
+		getLog().debug(content);
+	}
+
+	@Override
+	public void debug(final CharSequence content, final Throwable error) {
+		getLog().debug(content, error);
+	}
+
+	@Override
+	public void debug(final Throwable error) {
+		getLog().debug(error);
+	}
+
+	@Override
+	public boolean isInfoEnabled() {
+		return getLog().isInfoEnabled();
+	}
+
+	@Override
+	public void info(final CharSequence content) {
+		getLog().info(content);
+	}
+
+	@Override
+	public void info(final CharSequence content, final Throwable error) {
+		getLog().info(content, error);
+	}
+
+	@Override
+	public void info(final Throwable error) {
+		getLog().info(error);
+	}
+
+	@Override
+	public boolean isWarnEnabled() {
+		return getLog().isWarnEnabled();
+	}
+
+	@Override
+	public void warn(final CharSequence content) {
+		getLog().warn(content);
+	}
+
+	@Override
+	public void warn(final CharSequence content, final Throwable error) {
+		getLog().warn(content, error);
+	}
+
+	@Override
+	public void warn(final Throwable error) {
+		getLog().warn(error);
+	}
+
+	@Override
+	public boolean isErrorEnabled() {
+		return getLog().isErrorEnabled();
+	}
+
+	@Override
+	public void error(final CharSequence content) {
+		getLog().error(content);
+	}
+
+	@Override
+	public void error(final CharSequence content, final Throwable error) {
+		getLog().error(content, error);
+	}
+
+	@Override
+	public void error(final Throwable error) {
+		getLog().error(error);
 	}
 }
--- src/test/java/com/github/danielflower/mavenplugins/release/log/LogProviderTest.java
+++ src/test/java/com/github/danielflower/mavenplugins/release/log/LogProviderTest.java
@@ -1,26 +1,124 @@
 package com.github.danielflower.mavenplugins.release.log;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.apache.maven.plugin.logging.Log;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LogProviderTest {
 	private static final String TEST_STRING = "This is a test";
+	private final Throwable throwable = new Throwable();
 	private final Log log = mock(Log.class);
-	private final LogInvocationHandler handler = new LogInvocationHandler();
-	private final LogProvider provider = new LogProvider(handler);
+	private final LogProvider provider = new LogProvider();
+
+	@Before
+	public void setup() {
+		provider.setLog(log);
+	}
 
 	@Test(expected = IllegalStateException.class)
-	public void uninstallizedAccess() {
-		provider.get().info(TEST_STRING);
+	public void illegalStateWhenLogDelegateNotSet() {
+		provider.setLog(null);
+		provider.isDebugEnabled();
 	}
 
 	@Test
-	public void verifyCallDelegate() {
-		handler.setLog(log);
-		provider.get().info(TEST_STRING);
+	public void isDebugEnabled() {
+		when(log.isDebugEnabled()).thenReturn(true);
+		assertTrue(provider.isDebugEnabled());
+	}
+
+	@Test
+	public void debug() {
+		provider.debug(TEST_STRING);
+		verify(log).debug(TEST_STRING);
+	}
+
+	@Test
+	public void debugWithStringAndThrowable() {
+		provider.debug(TEST_STRING, throwable);
+		verify(log).debug(TEST_STRING, throwable);
+	}
+
+	@Test
+	public void debugWithThrowable() {
+		provider.debug(throwable);
+		verify(log).debug(throwable);
+	}
+
+	@Test
+	public void isInfoEnabled() {
+		when(log.isInfoEnabled()).thenReturn(true);
+		assertTrue(provider.isInfoEnabled());
+	}
+
+	@Test
+	public void info() {
+		provider.info(TEST_STRING);
 		verify(log).info(TEST_STRING);
+	}
+
+	@Test
+	public void infoWithStringAndThrowable() {
+		provider.info(TEST_STRING, throwable);
+		verify(log).info(TEST_STRING, throwable);
+	}
+
+	@Test
+	public void infoWithThrowable() {
+		provider.info(throwable);
+		verify(log).info(throwable);
+	}
+
+	@Test
+	public void isWarnEnabled() {
+		when(log.isWarnEnabled()).thenReturn(true);
+		assertTrue(provider.isWarnEnabled());
+	}
+
+	@Test
+	public void warn() {
+		provider.warn(TEST_STRING);
+		verify(log).warn(TEST_STRING);
+	}
+
+	@Test
+	public void warnWithStringAndThrowable() {
+		provider.warn(TEST_STRING, throwable);
+		verify(log).warn(TEST_STRING, throwable);
+	}
+
+	@Test
+	public void warnWithThrowable() {
+		provider.warn(throwable);
+		verify(log).warn(throwable);
+	}
+
+	@Test
+	public void isErrorEnabled() {
+		when(log.isErrorEnabled()).thenReturn(true);
+		assertTrue(provider.isErrorEnabled());
+	}
+
+	@Test
+	public void error() {
+		provider.error(TEST_STRING);
+		verify(log).error(TEST_STRING);
+	}
+
+	@Test
+	public void errorWithStringAndThrowable() {
+		provider.error(TEST_STRING, throwable);
+		verify(log).error(TEST_STRING, throwable);
+	}
+
+	@Test
+	public void errorWithThrowable() {
+		provider.error(TEST_STRING, throwable);
+		verify(log).error(throwable);
 	}
 }
