2f8f3c548a5979f19b1c58fc193e6152801b62c7
Gunnar Hilling <gunnar@hilling.de>
2017-04-05 10:56:19 +0200

    refactor test packages/java code.

--- test-projects/deep-dependencies/the-core-utilities/src/main/java/com/github/danielflower/mavenplugins/testprojects/versioninheritor/Calculator.java
+++
@@ -1,9 +0,0 @@
-package de.hilling.maven.release.testprojects.versioninheritor;
-
-public class Calculator {
-
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-}
--- test-projects/snapshot-dependencies-with-version-properties/src/main/java/com/github/danielflower/mavenplugins/testproject/snapshotdependencies/Main.java
+++
@@ -1,7 +0,0 @@
-package de.hilling.maven.release.testproject.snapshotdependencies;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
