3ff57b1bbb84b7228e4fb7596e387d67b4b46bbb
Gunnar Hilling <gunnar@hilling.de>
2017-04-05 14:00:36 +0200

    Revert: refactor test packages/java code.

---
+++ test-projects/module-with-test-failure/src/test/java/com/github/danielflower/mavenplugins/testproject/singlepom/MainTest.java
@@ -0,0 +1,10 @@
+package de.hilling.maven.release.testproject.singlepom;
+
+import org.junit.Assert;
+
+public class MainTest {
+    @org.junit.Test
+    public void aTestThatFails() throws Exception {
+        Assert.assertEquals("wave", "particle");
+    }
+}
