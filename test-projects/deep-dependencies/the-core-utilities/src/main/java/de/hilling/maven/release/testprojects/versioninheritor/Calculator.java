29c0afc9091a4faa26d03174c93d9dc8cfc41728
Gunnar Hilling <gunnar@hilling.de>
2017-04-05 14:04:51 +0200

    add new tests (move)

---
+++ test-projects/deep-dependencies/the-core-utilities/src/main/java/de/hilling/maven/release/testprojects/versioninheritor/Calculator.java
@@ -0,0 +1,9 @@
+package de.hilling.maven.release.testprojects.versioninheritor;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ test-projects/module-with-profiles/src/test/java/de/hilling/maven/release/testproject/singlepom/MainTest.java
@@ -0,0 +1,10 @@
+package de.hilling.maven.release.testproject.singlepom;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void aTestThatFails() throws Exception {
+        System.out.println("The module-with-profiles test has run");
+    }
+}
