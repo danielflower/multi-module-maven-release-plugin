b4082bc1393bbd69cf2cb2d37951fbc06fd26011
isc-msa <6095628+msaladin@users.noreply.github.com>
2024-11-11 09:14:37 +0100

    NOISSUE: Simplified test project to not use Spring

--- test-projects/openapi-spec-as-plugin-dependency/the-service-impl/src/main/java/com/github/danielflower/mavenplugins/testprojects/openapispecasplugindependency/OpenApiController.java
+++ test-projects/openapi-spec-as-plugin-dependency/the-service-impl/src/main/java/com/github/danielflower/mavenplugins/testprojects/openapispecasplugindependency/OpenApiController.java
@@ -2,19 +2,15 @@ package com.github.danielflower.mavenplugins.testprojects.versioninheritor;
 
 import com.github.danielflower.mavenplugins.tesetprojects.openapispecasplugindependency.facade.openapi.api.PingApi;
 import com.github.danielflower.mavenplugins.tesetprojects.openapispecasplugindependency.facade.openapi.model.PingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import java.time.OffsetDateTime;
-import org.springframework.http.ResponseEntity;
 
-@RestController
-@RequestMapping("/api")
-public class OpenApiController implements PingApi {
+public class OpenApiController {
 
-    @Override
-    public ResponseEntity<PingResult> ping() {
-        var result = new PingResult(OffsetDateTime.now());
-        return ResponseEntity.ok(result);
+
+    public static void main(String[] args) {
+        // Not really a controller, but at least it uses the classes PingResult
+        PingResult x = new PingResult();
     }
+
 
 }
